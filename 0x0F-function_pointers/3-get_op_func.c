53149f20fde4ca421066c375e8343963de32a2c7
Yayra91 <ryobi@st.ug.edu.gh>
2023-05-24 18:53:42 -0700

    Correction 2

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,32 @@
 #include "3-calc.h"
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
- * get_op_func - the Compares function with pointer and returns the equal.
- * @s: the points a char
- * Return: the Returns the function and the operator when equal
-**/
-
+  * get_op_func - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+	int i = 0;
 
-int i = 0;
+	while (i < 5)
+	{
+	if (strcmp(s, ops[i].op) == 0)
+	return (ops[i].f);
 
-while (ops[i].op != NULL)
-{
-if (!strcmp(ops[i].op, s))
-return (ops[i].f);
-i++;
-}
-return (NULL);
+	i++;
+	}
+
+	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -48,6 +50,11 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 /**
@@ -59,6 +66,11 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
+
 	return (a % b);
 }
-
